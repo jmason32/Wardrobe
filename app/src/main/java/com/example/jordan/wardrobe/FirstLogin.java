a40e733747c79d8cea77d0d6079f4059b2508caf
jmason32 <jordanmason32@gmail.com>
2016-08-07 01:10:24 -0500

    Simple login screen in (first_login)

---
+++ app/src/main/java/com/example/jordan/wardrobe/FirstLogin.java
@@ -0,0 +1,43 @@
+package com.example.jordan.wardrobe;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FirstLogin extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginbt;
+
+    //Creating global variables
+    //then are then referneced in the onCreate method
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        loginbt = (Button) findViewById(R.id.login);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
